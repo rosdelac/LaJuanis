95ddb6d0cbb34e6281f20f44b6fc8f6896157a05
codeamac02 <codeamac02@gmail.com>
2016-11-25 14:52:18 -0600

    segundo commit

---
+++ public/js/application.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('#tablaspedidos').hide();
+  $('#tablausuarios').hide();
+//==============================================================  
+$('body').on('submit','#infopedido',function(event){
+    event.preventDefault();
+    var cadena = $(this).serialize();
+    $.post('/pedidos',cadena,function(data){
+      $('#containerpedido').html(data);
+    });     
+  });
+//==============================================================
+$('body').on('click','#pedidosbtn',function(event){
+      $('#tablausuarios').hide();
+      $('#tablaspedidos').slideToggle();
+  });
+//==============================================================
+$('body').on('click','#usuariosbtn',function(event){
+      $('#tablaspedidos').hide();
+      $('#tablausuarios').slideToggle();
+  });
+//==============================================================
+});
