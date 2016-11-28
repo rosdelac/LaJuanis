d22f4abcb4e3f254ab50ad87cf2a676e5009ae10
rosdelac <rodrigosierradlc@gmail.com>
2016-11-28 08:41:56 -0600

    added admins

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  # La siguiente linea hace render de la vista 
+  # La siguiente linea hace render de la vista
   # que esta en app/views/index.erb
   erb :index
 end
@@ -24,13 +24,17 @@ get '/adminpanel' do
 end
 
 post '/pedidos' do
-  valor = true
+    valor = true
   if params[:promociones] == "true"
     valor = true
   else
     valor = false
   end
-  @user  = User.create(name: params[:nombre], email: params[:email], telephone: params[:telefono], promotions: valor)
+  @user  = User.find_by(email: params[:email])
+  if @user == nil
+    @user  = User.create(name: params[:nombre], email: params[:email], telephone: params[:telefono], promotions: valor)
+  else  
+  end
   @order = Order.create(user_id: @user.id, name: params[:nombre], email: params[:email], telephone: params[:telefono], date: params[:fecha])
   @cake  = Cake.create(order_id: @order.id, flavor: params[:flavor], size: params[:tamano], instructions: params[:instrucciones])
   erb :pedidofinal, layout: false
--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  $('#tablaspedidos').hide();
   $('#tablausuarios').hide();
-//==============================================================  
+//==============================================================
 $('body').on('submit','#infopedido',function(event){
     event.preventDefault();
     var cadena = $(this).serialize();
     $.post('/pedidos',cadena,function(data){
       $('#containerpedido').html(data);
-    });     
+    });
   });
 //==============================================================
 $('body').on('click','#pedidosbtn',function(event){
